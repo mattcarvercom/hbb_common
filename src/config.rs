e65e1a2688e4b3751b15b9374a29bddf8481f05f
chenzibo <qw.54@163.com>
2026-07-21 23:30:55 +0800

    feat: add custom web TLS path options

--- src/config.rs
+++ src/config.rs
@@ -2830,6 +2830,8 @@ pub mod keys {
     pub const OPTION_WEB_ACCESS_ENABLED: &str = "web-access-enabled";
     pub const OPTION_WEB_LISTEN_PORT: &str = "web-listen-port";
     pub const OPTION_WEB_HTTPS_ENABLED: &str = "web-https-enabled";
+    pub const OPTION_WEB_CERTIFICATE_PATH: &str = "web-certificate-path";
+    pub const OPTION_WEB_PRIVATE_KEY_PATH: &str = "web-private-key-path";
     pub const OPTION_DIRECT_SERVER: &str = "direct-server";
     pub const OPTION_ALLOW_WEBSOCKET: &str = "allow-websocket";
     pub const OPTION_TRACKPAD_SPEED: &str = "trackpad-speed";
